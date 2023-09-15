d455fd64c03d92e3544fbd6804d623a0626c65d8
divyapelluri <139614718+divyapelluri@users.noreply.github.com>
2023-09-15 23:43:55 +0530

    Add files via upload

---
+++ todolist-v1/date.js
@@ -0,0 +1,17 @@
+exports.getDate=function(){
+const today=new Date();
+    let options={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+    return day=today.toLocaleDateString("en-US",options);
+}
+
+exports.getDay=function(){
+    const today=new Date();
+        let options={
+            weekday:"long",
+        };
+        return day=today.toLocaleDateString("en-US",options);
+    }
